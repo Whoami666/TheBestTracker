2f748755bb71979ae236828bd095876b69e718f8
Tyulpakova Marina <105711011+Whoami66666@users.noreply.github.com>
2022-06-21 23:23:12 +0300

    Class For Visuals

---
+++ CategoryStuff/DatesView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestTracker.CategoryStuff.DataView
+{
+    public class DatesView
+    {
+        public string Name { get; set; }
+        public List<DateName> Dates { get; set; }
+        public DatesView(string name)
+        {
+            Name = name;
+            Dates = new List<DateName>();
+        }
+    }
+
+    public class DateName
+    {
+        public string Name { get; set; }
+        public DateName(int name)
+        {
+            Name = name.ToString();
+        }
+
+    }
+
+    
+    public class WeeksView
+    {
+        public string Name { get; set; }
+        public List<Category> DayCategory { get; set; }
+        public WeeksView(string name)
+        {
+            Name = name;
+            DayCategory = new List<Category>();
+        }
+
+
+    }
+}
